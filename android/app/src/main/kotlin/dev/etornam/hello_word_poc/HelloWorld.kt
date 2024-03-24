d65f9477e39115cd1e3a311a2842f919a19934b3
iamEtornam <sunumacbright@gmail.com>
2024-03-24 17:19:20 +0000

    add android integration

---
+++ android/app/src/main/kotlin/dev/etornam/hello_word_poc/HelloWorld.kt
@@ -0,0 +1,6 @@
+package dev.etornam.hello_word_poc
+
+object HelloWorld {
+    val message: String
+        get() = "Hello World from Android"
+}
--- android/app/src/main/kotlin/dev/etornam/hello_word_poc/MainActivity.kt
+++ android/app/src/main/kotlin/dev/etornam/hello_word_poc/MainActivity.kt
@@ -3,3 +3,18 @@ package dev.etornam.hello_word_poc
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+{
+    private val CHANNEL = "hello_world_poc/app"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call, result ->
+
+            if(call.method == "hello_world_poc") {
+                result.success(HelloWorld.get())
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
